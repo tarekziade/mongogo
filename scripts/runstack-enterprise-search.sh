fd5d3011c85cbcb697febb4ed0c6ca92d793ef12
cdelgado <carlos.delgado@elastic.co>
2022-06-02 15:29:23 +0200

    Removed unnecessary scripts

--- scripts/runstack-enterprise-search.sh
+++ scripts/runstack-enterprise-search.sh
@@ -5,9 +5,6 @@ docker-compose up -d
 
 docker-compose --profile enterprise-search up -d
 
-./wait-for-kibana.sh
 ./update-kibana-user-password.sh
-
-./wait-for-mongo.sh
 
 docker exec mongo1 /scripts/rs-init.sh
